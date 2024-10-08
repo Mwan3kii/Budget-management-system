fc82ebb10ab34bb41eef7cd9435174d9f45d0c23
Mwan3kii <agathawanja303@gmail.com>
2024-10-08 18:30:15 +0300

    Add login route in app.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
-import Register from './Authentication/Register';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Login from './Authentication/Login';
+import Register from './Authentication/Register';
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Router>
         <Routes>
           <Route path='/signup' element={<Register/>}/>
+          <Route path='/login' element={<Login/>}/>
         </Routes>
       </Router>
     </div>
