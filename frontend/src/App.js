f47daf4518b101c6d13851e1caa52b8d923880b3
Mwan3kii <agathawanja303@gmail.com>
2024-10-08 17:56:46 +0300

    Update app.js with route path

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,15 @@
 import './App.css';
+import Register from './Authentication/Register';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
+      <Router>
+        <Routes>
+          <Route path='/signup' element={<Register/>}/>
+        </Routes>
+      </Router>
     </div>
   );
 }
