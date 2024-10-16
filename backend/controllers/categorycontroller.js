a36a84621220ca6f17064407a9d22c881a0b26be
Mwan3kii <agathawanja303@gmail.com>
2024-10-16 18:50:00 +0300

    Function to delete specific category by Id

--- backend/controllers/categorycontroller.js
+++ backend/controllers/categorycontroller.js
@@ -23,3 +23,15 @@ exports.categoryDetails = catchAsyncError (async(req, res, next) => {
     res.status(200).json(category);
 });
 
+exports.deleteCategory = catchAsyncError (async(req, res, next) => {
+    console.log("Received data from frontend:", req.params);
+    const { id } = req.params;
+    
+    const category = await Category.destroy({
+        where: { id }
+    });
+    if (!category) {
+        return res.status(404).json({ message: 'Category not found' });
+    }
+    res.status(200).json({ message: 'Category deleted successfully', category });
+});
