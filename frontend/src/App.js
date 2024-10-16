e235b5dd02dae7da4322d96310d7b7c67599c731
Mwan3kii <agathawanja303@gmail.com>
2024-10-16 13:16:26 +0300

    Add category routes in app.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,11 @@ import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Login from './Authentication/Login';
 import Register from './Authentication/Register';
+import Login from './Authentication/Login';
+import HomePage from './Homepage/HomePage';
+import CategoryPage from './Homepage/CategoryPage';
+import CategoryDetails from './Homepage/CategoryDetails';
+import Menu from './Header/Menu';
 
 function App() {
   return (
@@ -10,6 +15,9 @@ function App() {
         <Routes>
           <Route path='/signup' element={<Register/>}/>
           <Route path='/login' element={<Login/>}/>
+          <Route path='/home' element={<HomePage/>}/>
+          <Route path="/category" element={<CategoryPage />} />
+          <Route path='/home/:id' element={<CategoryDetails/>}/>
         </Routes>
       </Router>
     </div>
