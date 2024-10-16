daa30246a545cfbe470bc18001671c307fcbf20c
Mwan3kii <agathawanja303@gmail.com>
2024-10-16 18:48:34 +0300

    Displays category details

--- backend/controllers/categorycontroller.js
+++ backend/controllers/categorycontroller.js
@@ -15,3 +15,11 @@ exports.getCategories = catchAsyncError (async(req, res, next) => {
     res.json(categories);
 });
 
+exports.categoryDetails = catchAsyncError (async(req, res, next) => {
+    const { id } = req.params;
+    const category = await Category.findOne({
+        where: { id }
+    });
+    res.status(200).json(category);
+});
+
