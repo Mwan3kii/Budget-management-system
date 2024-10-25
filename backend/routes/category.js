ba6f3484775bb46b6ff6f7a77e5116fa3000e613
Mwan3kii <agathawanja303@gmail.com>
2024-10-25 21:22:01 +0300

    Add user authenticated route before controller functions

--- backend/routes/category.js
+++ backend/routes/category.js
@@ -2,12 +2,12 @@ const express = require('express');
 const router = express.Router();
 
 const { createCategory, getCategories, categoryDetails, deleteCategory } = require('../controllers/categorycontroller');
+const { isAuthenticatedUser } = require('../middleware/auth');
 
-router.route('/category').post(createCategory);
-router.route('/home').get(getCategories);
-router.route('/home/:id').get(categoryDetails);
-router.route('/home/:id').delete(deleteCategory);
-
+router.route('/category').post(isAuthenticatedUser, createCategory);
+router.route('/home').get(isAuthenticatedUser, getCategories);
+router.route('/home/:id').get(isAuthenticatedUser, categoryDetails);
+router.route('/home/:id').delete(isAuthenticatedUser, deleteCategory);
 
 
 module.exports = router;
--- backend/routes/transaction.js
+++ backend/routes/transaction.js
@@ -2,8 +2,9 @@ const express = require('express');
 const router = express.Router();
 
 const { createTransaction, getTransactions } = require('../controllers/transactioncontroller');
+const { isAuthenticatedUser } = require('../middleware/auth');
 
-router.route('/home/:id/transaction').post(createTransaction);
-router.route('/home/:id').get(getTransactions);
+router.route('/home/:id/transaction').post(isAuthenticatedUser, createTransaction);
+router.route('/home/:id').get(isAuthenticatedUser, getTransactions);
 
 module.exports = router;
