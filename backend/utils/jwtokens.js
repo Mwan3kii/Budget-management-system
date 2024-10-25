a51c5c9304e05c2cba9442cc4c417cdde7db9fdb
Mwan3kii <agathawanja303@gmail.com>
2024-10-25 20:05:07 +0300

    Create token and set cookie option samesite strict

--- backend/utils/jwtokens.js
+++ backend/utils/jwtokens.js
@@ -1,12 +1,18 @@
+const dotenv = require('dotenv');
+dotenv.config({path: '../config/config.env'});
+
 const sendToken = (user, statusCode, res) => {
-    const expireTime = parseInt(process.env.COOKIE_EXPIRATION_TIME, 10);
+    // Create JWT token
     const token = user.getJwToken();
+    
+    // Set options for cookie
     const options = {
         expires: new Date(
-            Date.now() + expireTime * 24 * 60 * 60 * 1000
+            Date.now() + process.env.COOKIE_EXPIRES_TIME * 24 * 60 * 60 * 1000
         ),
         httpOnly: true,
         secure: process.env.NODE_ENV === 'DEVELOPMENT',
+        sameSite: 'strict'
     };
 
     res.status(statusCode).cookie('token', token, options).json({
