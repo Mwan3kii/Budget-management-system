c96ffcb2eadc889316fb59a65d5d0f18d242450a
Mwan3kii <agathawanja303@gmail.com>
2024-10-25 20:30:49 +0300

    Add slice func to Logout user from the session

--- frontend/src/Redux/Auth/LoginUser.js
+++ frontend/src/Redux/Auth/LoginUser.js
@@ -28,6 +28,25 @@ export const loginUser = createAsyncThunk('auth/login', async (userData) => {
     },
 )
 
+export const logout = createAsyncThunk('auth/logout', async () => {
+    const localUser = JSON.parse(localStorage.getItem('user logged-in'));
+    const accessToken = localUser && localUser.token;
+  
+    try {
+      const response = await axios.get('http://localhost:4000/api/v1/logout', {
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+        },
+      });
+      if (response.status === 200) {
+        localStorage.removeItem('user logged-in');
+      }
+      return null;
+    } catch (error) {
+      throw new Error(error.response.data.status || 'Logout failed');
+    }
+  });
+
 const loginSlice = createSlice({
     name: 'Login-user',
     initialState,
@@ -41,11 +60,18 @@ const loginSlice = createSlice({
                 state.user = action.payload;
                 state.loading = false;
                 state.success = true;
+                state.isAuthenticated = true;
             })
             .addCase(loginUser.rejected, (state, action) => {
                 state.loading = false;
                 state.success = false;
                 state.error = action.payload
+            })
+            .addCase(logout.fulfilled, (state)=>{
+                state.user = null;
+            })
+            .addCase(logout.rejected, (state, action) => {
+                state.error = true
             })
     }
 })
